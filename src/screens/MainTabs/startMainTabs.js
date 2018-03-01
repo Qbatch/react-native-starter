36e09200b1b24fece52d27ab3a8160e0db429519
Qbatch <qbatch@Qbatchs-iMac.local>
2018-03-01 13:46:13 +0500

    Build on Android, VectorIcons & Navigation

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,42 +1,52 @@
 import { Navigation } from 'react-native-navigation';
 
-// import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-    Navigation.startTabBasedApp({
-        tabs: [
-            {
-                screen: "react-native-starter.FindPlaceScreen",
-                label: "Find Place",
-                title: "Find Place",
-                navigatorButtons: {
-                    leftButtons: [
-                        {
-                            title: "Menu",
-                            id: "sideDrawerToggle"
-                        }
-                    ]
+    Promise.all([
+        Icon.getImageSource("md-map", 30),
+        Icon.getImageSource("ios-share-alt", 30),
+        Icon.getImageSource("ios-menu", 30),
+    ]).then(sources => {
+        Navigation.startTabBasedApp({
+            tabs: [
+                {
+                    screen: "react-native-starter.FindPlaceScreen",
+                    label: "Find Place",
+                    title: "Find Place",
+                    icon: sources[0],
+                    navigatorButtons: {
+                        leftButtons: [
+                            {
+                                title: "Menu",
+                                icon: sources[2],
+                                id: "sideDrawerToggle"
+                            }
+                        ]
+                    }
+                },
+                {
+                    screen: "react-native-starter.SharePlaceScreen",
+                    label: "Share Place",
+                    title: "Share Place",
+                    icon: sources[1],
+                    navigatorButtons: {
+                        leftButtons: [
+                            {
+                                title: "Menu",
+                                icon: sources[2],
+                                id: "sideDrawerToggle"
+                            }
+                        ]
+                    }
                 }
-            },
-            {
-                screen: "react-native-starter.SharePlaceScreen",
-                label: "Share Place",
-                title: "Share Place",
-                navigatorButtons: {
-                    leftButtons: [
-                        {
-                            title: "Menu",
-                            id: "sideDrawerToggle"
-                        }
-                    ]
+            ],
+            drawer: {
+                left: {
+                    screen: "react-native-starter.SideDrawer"
                 }
             }
-        ],
-        drawer: {
-            left: {
-                screen: "react-native-starter.SideDrawer"
-            }
-        }
+        });
     });
 };
 
--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -4,7 +4,7 @@ import { View, Text, Dimensions, StyleSheet } from 'react-native';
 class SideDrawer extends Component {
     render () {
         return(
-            <View style={[ styles.container, { width: Dimensions.get("window").width * 0.5 } ]}>
+            <View style={[ styles.container, { width: Dimensions.get("window").width * 0.8 } ]}>
                 <Text>SideDrawer</Text>
             </View>
         );
